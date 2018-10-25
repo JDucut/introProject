e6a76f740f27de171856610c02f12b802ca17e4c
JDucut <jeremiahducut@gmail.com>
2018-10-25 22:18:19 +0000

    added validation to the school table

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,6 @@
 class School < ApplicationRecord
 	has_many :spells
+
+	validation :schoolName, presense: true
+	validation :description, presense: true
 end
--- app/models/spell.rb
+++ app/models/spell.rb
@@ -3,6 +3,6 @@ class Spell < ApplicationRecord
 	has_many :class_spell_lists
 	has_many :adventurer_classes, through: :class_spell_lists
 
-	validates :schoolName, presence: true
+	validates :name, presence: true
 	validates :description, presence: true
 end
