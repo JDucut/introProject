000a1fae5cb70216bb55c2196804212b452a6359
JDucut <jeremiahducut@gmail.com>
2018-10-25 22:15:41 +0000

    added validation to the spell table

--- app/models/class_spell_list.rb
+++ app/models/class_spell_list.rb
@@ -1,4 +1,7 @@
 class ClassSpellList < ApplicationRecord
   belongs_to :adventurer_class
   belongs_to :spell
+
+  validates :adventurer_class presence: true
+  validates :spell presence: true
 end
--- app/models/spell.rb
+++ app/models/spell.rb
@@ -2,4 +2,7 @@ class Spell < ApplicationRecord
 	belongs_to :school
 	has_many :class_spell_lists
 	has_many :adventurer_classes, through: :class_spell_lists
+
+	validates :schoolName, presence: true
+	validates :description, presence: true
 end
