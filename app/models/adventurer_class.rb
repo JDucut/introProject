c3593cc51e8e501f32b7a070894dc347db581ae8
JDucut <jeremiahducut@gmail.com>
2018-10-25 18:46:57 +0000

    added validation to the adventurer class table

--- app/models/adventurer_class.rb
+++ app/models/adventurer_class.rb
@@ -1,4 +1,7 @@
 class AdventurerClass < ApplicationRecord
 	has_many :class_spell_lists
 	has_many :spells, through: :class_spell_lists
+
+	validates :name, presence: true
+	validates :description, presence: true
 end
