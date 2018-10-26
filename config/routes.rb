6289fbab1a93bfae8e846069a91fd6d3cc18ed32
JDucut <jeremiahducut@gmail.com>
2018-10-26 04:08:29 +0000

    started working on search controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  
-  # get 'adventurer_classes/index'
-  # get 'adventurer_classes/show'
-  # get 'school/index'
-  # get 'school/show'
+
 	resources :spells, only: [:index, :show]
 	root to: 'spells#index'
 
@@ -12,7 +8,4 @@ Rails.application.routes.draw do
 
 	resources :adventurer_classes, only: [:index, :show]
 	root to: 'adventurer_classes#index'
-  # get 'spells' => 'spells#index'
-  # get 'spells/:name', to: 'spells#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
